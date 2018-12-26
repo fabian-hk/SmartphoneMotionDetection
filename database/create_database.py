65888f37c3de80ba8374e1d41c23b9eba402a5db
Fabian Hauck <hauckfabian@gmail.com>
2018-12-26 14:56:42 +0100

    removed .db files from repository

--- database/create_database.py
+++ database/create_database.py
@@ -2,6 +2,8 @@ import sqlite3
 from DataLoader import DataLoader
 import json
 
+dataset_path = "C:/Users/Fabian/OneDrive - bwedu/Privat/MotionDetection_Dataset/SensorCollector_Time_Gravity/"
+
 connection = sqlite3.connect("data.db")
 cursor = connection.cursor()
 
@@ -10,7 +12,7 @@ cursor.execute("""DROP TABLE data;""")
 sql_create = """CREATE TABLE data (id INTEGER PRIMARY KEY, type INTEGER, class INTEGER, data TEXT);"""
 cursor.execute(sql_create)
 
-loader = DataLoader("data/", 4, 1, [0.8, 0.15])
+loader = DataLoader(dataset_path, 4, 1, [0.8, 0.15])
 
 for type in range(3):
     for i in range(loader.length(type)):
